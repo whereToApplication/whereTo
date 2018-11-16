524c14cae0be4464ef5bf616a77c92527204baaf
iecurius <saahilyechuri@gmail.com>
2018-11-16 14:13:47 -0500

    Post feedback fixed; runs on different Thread

--- app/src/main/java/slicksoala/wheretoapp/FeedbackForm.java
+++ app/src/main/java/slicksoala/wheretoapp/FeedbackForm.java
@@ -1,9 +1,12 @@
 package slicksoala.wheretoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.OutputStream;
 import java.net.URL;
@@ -12,7 +15,7 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class FeedbackForm extends AppCompatActivity {
 
-    final static String url = "http://where2trip.herokuapp.com/feedback";
+    final static String url = "https://where2trip.herokuapp.com/feedback";
     EditText feedbackTxt;
     Button submitBtn;
 
@@ -22,18 +25,28 @@ public class FeedbackForm extends AppCompatActivity {
         setContentView(R.layout.activity_feedback_form);
         feedbackTxt = findViewById(R.id.feedbackText);
         submitBtn = findViewById(R.id.submitBtn);
+
+        submitBtn.setOnClickListener(v -> submitFeedback());
     }
 
     public void submitFeedback() {
         String feedback = feedbackTxt.getText().toString();
-        try {
-            sendPost(feedback);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Thread postThread = new Thread(() -> {
+            try  {
+                sendPost(feedback);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        postThread.start();
+        Toast.makeText(FeedbackForm.this,
+                "Feedback received! Thank you for your inputs!",
+                Toast.LENGTH_LONG).show();
+        Intent backIntent = new Intent(this, DetailsForm.class);
+        startActivity(backIntent);
     }
 
-    public static void sendPost(String feedback) throws Exception {
+    public void sendPost(String feedback) throws Exception {
         URL obj = new URL(url);
         HttpsURLConnection connection = (HttpsURLConnection) obj.openConnection();
         connection.setRequestMethod("POST");
